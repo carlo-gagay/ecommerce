502cf2d2d510bf306d06e685a68b750a4dc5738d
carlokidz <tabayagcarlo94@gmail.com>
2023-08-21 09:49:10 +0800

    Top header creation

---
+++ resources/js/Components/General/TopHeader.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="top-header">
+        <div class="inner">
+            <div class="start-section">
+                <div class="branding">
+                    
+                </div>
+                <div class="nav-links">
+
+                </div>
+            </div>
+            <div class="end-section">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+#top-header {
+    @apply p-2;
+
+    .inner {
+        @apply flex justify-between items-center;
+
+        .start-section {
+            @apply flex items-center;
+
+            .branding {
+
+            }
+
+            .nav-links {
+
+            }
+        }
+
+        .end-section {
+
+        }
+    }
+}
+</style>
